0ec7274a447b2095ceb9b32b559ab3925303ad3b
KentBurgos18 <104112603+KentBurgos18@users.noreply.github.com>
2022-04-25 23:34:54 -0500

    Delete ejercicio1.js

--- ejercicio1.js
+++
@@ -1,8 +0,0 @@
-const tablaMultiplicar =function(N,i=1){
-    if(i<=10){
-        console.log(`${N} * ${i} = ${N*i}`)
-        i++;
-        return tablaMultiplicar(N,i=i)
-    }
-}
-tablaMultiplicar(7);
